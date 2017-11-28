700bd659b85812be0fa33df83e8102aa4d670edd
Maxime Blanchard <max@Maximes-MacBook-Pro.local>
2017-11-28 16:20:47 -0500

    major changes

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 var selectOutColor = document.getElementById('outsidecolor');
 var selectInColor = document.getElementById('insidecolor');
 
+//Initialisation des variables nb de Cup et layer de café
+var layerCounter = 6;
+var cupCount = 0;
+
 //Selection des Boutons
+var allButtons = document.getElementsByClassName('buttons');
 var btnCreate = document.getElementById('create');
 var btnFillCup = document.getElementById('fill');
 var btnTakeSip = document.getElementById('sip');
@@ -32,6 +37,13 @@ function init() {
   btnDestroy.addEventListener('click', destroyCup);
   btnGaucher.addEventListener('click', switchSide);
   btnDroitier.addEventListener('click', switchSide);
+  disableButtons();
+}
+
+function disableButtons() {
+  for (var i=1; i < allButtons.length; i++) {
+    allButtons[i].disabled = true;
+  }
 }
 
 function createCup() {
@@ -43,13 +55,19 @@ function createCup() {
     for (var j=0; j < coffeeLayers.length; j++) {
       coffeeLayers[j].style.backgroundColor = selectInColor.value;
     }
+    for (var k=1; k < allButtons.length; k++) {
+      allButtons[k].disabled = false;
+    }
+    btnEmptyCup.disabled = true;
+    btnTakeSip.disabled = true;
+    btnCreate.disabled = true;
+    btnDroitier.disabled = true;
     cupDiv.style.visibility = 'visible';
   } else {
     alert("Vous avez déjà une tasse");
   }
 }
 
-var cupCount = 0;
 function cupCounter() {
   cupCount++;
   cupNumber.textContent = cupCount;
@@ -59,14 +77,25 @@ function fillCup() {
   while (layerCounter < 7 && layerCounter >= 0) {
     addCoffee();
   }
+  if (layerCounter == -1) {
+    btnFillCup.disabled = true;
+    btnEmptyCup.disabled = false;
+    btnAddLayer.disabled = true;
+    btnTakeSip.disabled = false;
+  }
 }
 
 function takeSip() {
-  if (layerCounter < 6) {
+  if (layerCounter < 5) {
     coffeeLayers[layerCounter+1].style.backgroundColor = selectInColor.value;
     layerCounter++;
-  } else {
-    alert("La tasse est vide");
+    btnFillCup.disabled = false;
+    btnAddLayer.disabled = false;
+  } else if (layerCounter == 5) {
+    coffeeLayers[layerCounter+1].style.backgroundColor = selectInColor.value;
+    layerCounter++;
+    btnTakeSip.disabled = true;
+    btnEmptyCup.disabled = true;
   }
 }
 
@@ -74,18 +103,29 @@ function emptyCup() {
   while (layerCounter < 6) {
     takeSip();
   }
+  if (layerCounter == 6) {
+    btnEmptyCup.disabled = true;
+    btnFillCup.disabled = false;
+    btnTakeSip.disabled = true;
+  }
 }
 
-var layerCounter = 6;
 var cost = 0.00;
 function addCoffee() {
-  if (layerCounter >= 0) {
+  if (layerCounter > 0) {
+    coffeeLayers[layerCounter].style.backgroundColor = "#1C0702";
+    layerCounter--;
+    cost = cost + parseFloat(0.75);
+    price.textContent = parseFloat(cost).toFixed(2);
+    btnTakeSip.disabled = false;
+    btnEmptyCup.disabled = false;
+  } else if (layerCounter == 0) {
     coffeeLayers[layerCounter].style.backgroundColor = "#1C0702";
     layerCounter--;
     cost = cost + 0.75;
     price.textContent = cost;
-  } else {
-    alert("Vous ne pouvez pas faire déborder la tasse. L’action d’ajout est annulée")
+    btnAddLayer.disabled = true;
+    btnFillCup.disabled = true;
   }
 }
 
@@ -93,6 +133,8 @@ function destroyCup() {
   cupDiv.style.visibility = '';
   cost = 0.00;
   price.textContent = cost;
+  disableButtons();
+  btnCreate.disabled = false;
 }
 
 function switchSide() {
@@ -100,9 +142,34 @@ function switchSide() {
     if (event.target.id == 'gaucher') {
       leftHandle[i].style.visibility = "visible";
       rightHandle[i].style.visibility = "hidden";
+      btnDroitier.disabled = false;
+      btnGaucher.disabled = true;;
     } else {
       rightHandle[i].style.visibility = '';
       leftHandle[i].style.visibility = '';
+      btnGaucher.disabled = false;
+      btnDroitier.disabled = true;
     }
   }
 }
+
+//Set Clock
+var secHand = document.getElementById('second-hand')
+var minHand = document.getElementById('min-hand')
+var hourHand = document.getElementById('hour-hand');
+
+function setDate() {
+  var now = new Date();
+  var sec = now.getSeconds();
+  var min = now.getMinutes();
+  var hour = now.getHours();
+  if (sec < 11) {
+    secHand.textContent = '0' + sec;
+  } else {
+    secHand.textContent = sec;
+  }
+  minHand.textContent = min;
+  hourHand.textContent = hour;
+}
+
+setInterval(setDate, 1000);
